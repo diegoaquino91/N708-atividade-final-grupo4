f800668769fa14141fd69104afdb402ea9017e21
Diego Aquino <di.aquino91@gmail.com>
2024-11-11 11:21:47 -0300

    atualizando aplicação

---
+++ listener-service/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"listener/event"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	// try to connect to rabbitmq
+	rabbitConnm, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConnm.Close()
+	
+	// start listening for messages
+	log.Println("Listening for and consuming RabbitMQ messages")
+	// create consumer
+	consume, err := event.NewConsumer(rabbitConnm)
+	if err != nil {
+		panic(err)
+	}
+	// watch the queue and consume events
+	err = consume.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	//don't continue until rabbit is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			log.Println("Connected to RabbitMQ!")
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off...")
+		time.Sleep(backOff)
+		continue
+	}
+
+	return connection, nil
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Projeto Aplicado Multiplataforma N708 - Atividade Final
+
+**Trabalhando com Microserviços em GO!**
